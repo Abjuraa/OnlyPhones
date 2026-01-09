5570c076e4c68572d451b37cf362c78d24c88a9b
Abjura <107774277+Abjuraa@users.noreply.github.com>
2026-01-09 14:45:19 -0500

    style se modifico los parametros del mapa para poder poner un marcador indicando el lugar de la tienda ademas se ajustaron los estilos para no tener conflictos con la tarjet

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,47 @@
-import { Map, MapControls } from "../components/ui/map";
-
+import { useState } from "react";
+import { Map, MapControls, MapMarker, MarkerContent } from "../components/ui/map";
+import { MapPin } from "lucide-react";
 
 export function MyMap() {
+
+    const [draggableMarker, setDraggableMarker] = useState({
+        lng: -74.08085,
+        lat: 4.60420,
+    })
     return (
-        <Map center={[-74.08175, 4.60971]} zoom={10}
+        <Map center={[-74.08086, 4.60421]} zoom={15}
             styles={{
                 light: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
             }}
         >
-            <MapControls
-                position="top-left"
-                showZoom
-                showCompass
-                showFullscreen
-            />
+            <MapMarker
+                draggable
+                longitude={draggableMarker.lng}
+                latitude={draggableMarker.lat}
+                onDragEnd={(lngLat) => {
+                    setDraggableMarker({
+                        lng: lngLat.lng,
+                        lat: lngLat.lat
+                    })
+                }}
+            >
+                <MarkerContent>
+                    <div className="cursor-move">
+                        <MapPin
+                            className="fill-black stroke-white dark:fill-white"
+                            size={28}
+                        />
+                    </div>
+                </MarkerContent>
+
+                <MapControls
+                    position="top-left"
+                    showZoom
+                    showCompass
+                    showLocate
+                    showFullscreen
+                ></MapControls>
+            </MapMarker>
         </Map>
     );
 }
--- src/pages/user/Find.jsx
+++ src/pages/user/Find.jsx
@@ -11,27 +11,28 @@ function Find() {
             </div>
 
             <div className="flex flex-row w-full pt-5">
-                <div className="flex flex-col border rounded-xl border-slate-200">
-                    <div className="flex py-3 px-3">
-                        <Tienda />
-                    </div>
-                    <div className="flex min-w-[300px]">
-                        <div className="flex flex-col w-full pb-3">
-                            <h1 className="font-semibold text-lg mt-2 px-3 pb-2">Centro Comercial Punto 13</h1>
-                            <div className="flex flex-col px-3">
-                                <p className="text-slate-500 text-sm">Calle 13 # 15-27, Bogotá</p>
-                                <p className="text-sm">Lun - Sab: <span className="text-slate-500">10AM - 8PM</span></p>
-                                <p className="text-sm">Dom: <span className="text-slate-500">10AM - 4PM</span></p>
-                            </div>
-                            <div className="flex w-full justify-center py-3 px-3">
-                                <button type="button" className="flex bg-slate-200 w-full justify-center items-center gap-2 py-2 rounded-xl text-sm font-semibold hover:bg-slate-300 transition duration-300 ease-in-out"> <IconLocation /> Cómo llegar</button>
+                <div className="flex flex-col">
+                    <div className="flex flex-col border rounded-xl border-slate-200">
+                        <div className="flex py-3 px-3">
+                            <Tienda />
+                        </div>
+                        <div className="flex min-w-[300px]">
+                            <div className="flex flex-col w-full pb-3">
+                                <h1 className="font-semibold text-lg mt-2 px-3 pb-2">Centro Comercial Punto 13</h1>
+                                <div className="flex flex-col px-3">
+                                    <p className="text-slate-500 text-sm">Calle 13 # 15-27, Bogotá</p>
+                                    <p className="text-sm">Lun - Sab: <span className="text-slate-500">10AM - 8PM</span></p>
+                                    <p className="text-sm">Dom: <span className="text-slate-500">10AM - 4PM</span></p>
+                                </div>
+                                <div className="flex w-full justify-center py-3 px-3">
+                                    <button type="button" className="flex bg-slate-200 w-full justify-center items-center gap-2 py-2 rounded-xl text-sm font-semibold hover:bg-slate-300 transition duration-300 ease-in-out"> <IconLocation /> Cómo llegar</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-1 h-[300px] ps-10">
+                <div className="flex h-[350px] w-[900px] ps-10">
                     <MyMap />
-
                 </div>
             </div>
         </div>
