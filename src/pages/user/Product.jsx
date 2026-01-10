134871873ba87f82a21f27725cb6ee5e5bfb4d9c
Abjura <107774277+Abjuraa@users.noreply.github.com>
2026-01-09 22:03:42 -0500

    style se estan cambiando los estilos por cada producto

--- src/pages/user/Product.jsx
+++ src/pages/user/Product.jsx
@@ -22,17 +22,51 @@ export default function Product() {
 
     console.log(productById)
     return (
-        <div className="flex flex-col">
-            <div className="flex flex-row w-screen justify-between items-center px-10 py-5">
-                <h1 className="text-5xl font-semibold">{productById.model}</h1>
-                <div className="flex flex-col items-end">
+        <div className="flex flex-1 flex-col">
+            <div className="flex flex-row justify-between items-center px-5 py-5">
+                <div className="flex text-sm">
+                    <a
+                        className="font-semibold text-gray-500"
+                        href="/privada/categorias"
+                    >
+                        Catálogo
+                    </a>
+                    <h1 className="font-semibold text-gray-500"> <span className="px-2">></span>{productById.model}</h1>
+                </div>
+                <div className="flex flex-row gap-3">
+                    <h1 className="bg-green-200 rounded-lg p-1 text-sm text-green-800 font-semibold">{productById.hasAvailable ? 'Disponible' : 'No disponible'}</h1>
+                    <h1 className="bg-gray-200 rounded-lg p-1 text-sm font-semibold">{productById.unitsAvailable == 1 ? "Unidad Única" : `${productById.unitsAvailable} Unidades`}</h1>
+                </div>
+            </div>
+            <div className="flex flex-row justify-between px-5 pt-5">
+                <div className="flex rounded-lg basis-192">
+                    <img src={productById.image} alt="" className="w-150 rounded-lg" />
+                </div>
+
+                <div className="flex flex-col justify-start w-2/4 py-5 px-2 gap-1 basis-128">
+                    <h1 className="text-4xl font-semibold ">{productById.model}</h1>
+                    <h1 className="text-lg text-gray-500">{productById.color}  •  {productById.capacity}</h1>
                     {productById.hasDiscount
-                        ? <>
-                            <h1 className="text-3xl font-semibold">${formatNumber(priceWhitDiscount(productById.price, productById.discount))}</h1>
-                            <p className="text-xl text-gray-400 font-semibold line-through">${formatNumber(productById.price)}</p>
-                        </>
-                        : <h1 className="text-3xl font-semibold">${formatNumber(productById.price)}</h1>
+                        ? (
+                            <div className="flex gap-5 items-end pt-3 pb-3">
+                                <h1 className="text-4xl font-semibold">${formatNumber(priceWhitDiscount(productById.price, productById.discount))}</h1>
+                                <h1 className="flex line-through text-lg text-gray-500">${formatNumber(productById.price)}</h1>
+                            </div>
+                        )
+                        : (
+                            <div className="flex pt-3 pb-3">
+                                <h1 className="text-4xl font-semibold">${formatNumber(productById.price)}</h1>
+                            </div>
+                        )
                     }
+                    <div className="flex border-t pb-4"></div>
+
+                    <div className="flex grid grid-cols-2 grid-rows-2 gap-5">
+                        <div className="border rounded-lg ">1</div>
+                        <div className="border rounded-lg p-5 ">2</div>
+                        <div className="border rounded-lg p-5 ">3</div>
+                        <div className="border rounded-lg p-5 ">4</div>
+                    </div>
                 </div>
             </div>
         </div>
